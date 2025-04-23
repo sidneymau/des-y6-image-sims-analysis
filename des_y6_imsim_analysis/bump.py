3c19c011b523c1c85f52f3b9fd4dd0d02d62342e
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-04-23 13:43:55 -0500

    fix: put back bump model

--- des_y6_imsim_analysis/bump.py
+++ des_y6_imsim_analysis/bump.py
@@ -179,7 +179,7 @@ def model(
         params["w"] = numpyro.sample("w", dist.LogNormal(np.log(0.1), 0.1))
     for i in range(4):
         if f"g_b{i}" not in fixed_param_values:
-                # std of softlaplace is pi/2 * scale, so to set std to a value V, we need
+            # std of softlaplace is pi/2 * scale, so to set std to a value V, we need
             # to set the scale to V * 2/pi
             # we use V = 1
             params[f"g_b{i}"] = numpyro.sample(f"g_b{i}", dist.SoftLaplace(0.0, 1 * 2.0 / jnp.pi))
