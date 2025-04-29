f9d27517f6f5760301bedcb4e265a5ba9d036da9
beckermr <becker.mr@gmail.com>
2025-04-29 14:24:24 -0500

    feat: add v4 model tests and comps to v2

--- des_y6_imsim_analysis/tests/test_integration.py
+++ des_y6_imsim_analysis/tests/test_integration.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import scipy.stats
 
-from des_y6_imsim_analysis import bump, interpolant
+from des_y6_imsim_analysis import bump, interpolant, interpolant_variants
 from des_y6_imsim_analysis.stats import (
     compute_model_chi2_info,
     measure_map,
@@ -373,4 +373,201 @@ def test_integration_interpolant_mcmc(capsys, model_kind):
             true_params[k],
             rtol=0,
             atol=5e-3,
+        )
+
+
+def _make_fake_interpolant_variants_data(*, num_pts, model_kind, seed, add_noise=False):
+    """Make fake data with an F(z) model."""
+
+    assert model_kind == "fgcconv"
+
+    rng = np.random.default_rng(seed)
+    true_params = {}
+    for i in range(4):
+        for j in range(num_pts):
+            true_params[f"f{j}_b{i}"] = rng.uniform(0, 1)
+    true_params["grl"] = rng.uniform(0, 1) * 0.1
+    true_params["grh"] = rng.uniform(0, 1) * 0.1
+
+    z = np.asarray(GMODEL_COSMOS_Z.copy()[1:-1], dtype=np.float64)
+
+    mns = (0.5, 0.7, 0.9, 1.1)
+    nzs = []
+    for mn in mns:
+        nzs.append(scipy.stats.lognorm.pdf(z / mn, 0.3))
+        nzs[-1] = nzs[-1] / np.sum(nzs[-1])
+    nzs = np.array(nzs, dtype=np.float64)
+
+    zbins = np.array(
+        [[ZBIN_LOW[0], ZBIN_HIGH[-1]]]
+        + [[ZBIN_LOW[i], ZBIN_HIGH[i]] for i in range(ZBIN_LOW.shape[0])],
+        dtype=np.float64,
+    )
+
+    mn_pars = []
+    for sind in range(-1, 10):
+        for bind in range(4):
+            mn_pars.append((sind, bind))
+    mn_pars = np.array(mn_pars, dtype=np.int32)
+
+    mn = interpolant_variants.model_mean(
+        n_pts=num_pts,
+        z=z,
+        nz=nzs,
+        mn_pars=mn_pars,
+        zbins=zbins,
+        params=true_params,
+        model_kind=model_kind,
+    )
+    efac = 1e-2
+    cov = np.diagflat((mn * efac) ** 2)
+    if add_noise:
+        mn += rng.normal(size=mn.shape) * np.sqrt(np.diag(cov))
+
+    model_data = interpolant_variants.make_model_data(
+        z=z,
+        nzs=nzs,
+        mn=mn,
+        cov=cov,
+        mn_pars=mn_pars,
+        zbins=zbins,
+        fixed_param_values={},
+        num_pts=num_pts,
+        model_kind=model_kind,
+    )
+
+    data = ModelData(
+        z=z,
+        nzs=nzs,
+        mn_pars=mn_pars,
+        zbins=zbins,
+        mn=mn,
+        cov=cov,
+    )
+
+    return {
+        "model_data": model_data,
+        "true_params": true_params,
+        "data": data,
+    }
+
+
+@pytest.mark.parametrize("model_kind", ["fgcconv"])
+def test_integration_interpolant_variants_map(model_kind):
+    fake_data = _make_fake_interpolant_variants_data(
+        num_pts=9, model_kind=model_kind, seed=42
+    )
+
+    model_data = fake_data["model_data"]
+    true_params = fake_data["true_params"]
+
+    map_params = measure_map(
+        model_module=interpolant_variants,
+        model_data=model_data,
+        seed=42,
+        learning_rate=1e-2,
+        num_steps=50_000,
+        progress_bar=True,
+    )
+
+    for k in set(map_params.keys()) | set(true_params.keys()):
+        assert k in map_params
+        assert k in true_params
+        np.testing.assert_allclose(
+            map_params[k],
+            true_params[k],
+            rtol=1e-2,
+            atol=1e-2,
+            err_msg=k,
+        )
+
+
+@pytest.mark.parametrize("model_kind", ["fgcconv"])
+@pytest.mark.parametrize("add_noise", [True, False])
+def test_integration_interpolant_variants_map_chi2(add_noise, model_kind):
+    fake_data = _make_fake_interpolant_variants_data(
+        num_pts=9, model_kind=model_kind, seed=42, add_noise=add_noise
+    )
+
+    model_data = fake_data["model_data"]
+    data = fake_data["data"]
+
+    if add_noise:
+        map_params = measure_map(
+            model_module=interpolant_variants,
+            model_data=model_data,
+            seed=654,
+            learning_rate=1e-2,
+            num_steps=100_000,
+            progress_bar=True,
+        )
+    else:
+        map_params = fake_data["true_params"]
+
+    chi2_info = compute_model_chi2_info(
+        model_module=interpolant_variants,
+        model_data=model_data,
+        data=data,
+        params=map_params,
+    )
+    if not add_noise:
+        assert np.allclose(chi2_info["chi2"], 0.0), chi2_info
+    else:
+        assert np.abs(chi2_info["p_value"] - 0.5) < 0.45, chi2_info
+
+
+@pytest.mark.parametrize("model_kind", ["fgcconv"])
+def test_integration_interpolant_variants_mcmc(capsys, model_kind):
+    with capsys.disabled():
+        fake_data = _make_fake_interpolant_variants_data(
+            num_pts=9, model_kind=model_kind, seed=42
+        )
+
+        model_data = fake_data["model_data"]
+        true_params = fake_data["true_params"]
+
+        map_params = measure_map(
+            model_module=interpolant_variants,
+            model_data=model_data,
+            seed=42,
+            learning_rate=1e-2,
+            num_steps=100_000,
+            progress_bar=True,
+        )
+
+        mcmc = run_mcmc(
+            model_module=interpolant_variants,
+            model_data=model_data,
+            init_params=map_params,
+            seed=42,
+            progress_bar=True,
+            dense_mass=True,
+            num_samples=2000,
+            num_chains=2,
+        )
+
+    mcmc.print_summary(exclude_deterministic=True)
+
+    for line in capsys.readouterr().out.splitlines():
+        if not line.strip():
+            continue
+        if "mean" in line:
+            continue
+        if "divergences" in line:
+            continue
+
+        parts = line.strip().split()
+        rhat = float(parts[-1])
+        assert np.abs(rhat - 1.0) < 0.01, line
+
+    samples = mcmc.get_samples()
+    for k in set(samples.keys()) | set(true_params.keys()):
+        assert k in samples
+        assert k in true_params
+        np.testing.assert_allclose(
+            np.mean(samples[k]),
+            true_params[k],
+            rtol=0.5,
+            atol=0.10,
+            err_msg=k,
         )
---
+++ notebooks/2025_04_22_modeling_v3/run_v4_modeling.py
@@ -0,0 +1,266 @@
+import jax  # noqa: I001
+
+jax.config.update("jax_enable_x64", True)
+
+import numpyro  # noqa: E402, I001
+
+numpyro.set_host_device_count(4)
+
+import contextlib  # noqa: E402, I001
+import hashlib  # noqa: E402, I001
+import io  # noqa: E402, I001
+import os  # noqa: E402, I001
+
+import h5py  # noqa: E402, I001
+import numpy as np  # noqa: E402, I001
+import tqdm  # noqa: E402, I001
+import yaml  # noqa: E402, I001
+
+from des_y6_imsim_analysis import interpolant_variants  # noqa: E402, I001
+from des_y6_imsim_analysis.stats import (  # noqa: E402, I001
+    compute_model_chi2_info,
+    measure_map,
+    run_mcmc,
+)
+from des_y6_imsim_analysis.utils import (  # noqa: E402, I001
+    compute_eff_nz_from_data,
+    read_data,
+)
+
+WRITE = True
+VERSION = "4.0"
+
+fit_stats = {}
+fit_data = {}
+
+model_module = interpolant_variants
+models = [
+    {
+        "num_pts": 9,
+        "model_kind": "fgcconv",
+    }
+]
+fnames = [
+    "../../data/N_gamma_alpha_v3_statistical.hdf5",
+    "../../data/N_gamma_alpha_v3_statistical-occupancy.hdf5",
+    "../../data/N_gamma_alpha_v3_statistical-neighbor.hdf5",
+    "../../data/N_gamma_alpha_v3_statistical-nz.hdf5",
+]
+keys = [os.path.basename(fname).replace(".hdf5", "").split("_")[-1] for fname in fnames]
+
+with tqdm.tqdm(ncols=80, total=len(models) * len(keys), desc="fitting models") as pbar:
+    for model in models:
+        model_kind = model["model_kind"]
+        seeds = [
+            (
+                abs(
+                    int(
+                        hashlib.sha256(
+                            (key + str(model_kind)).encode("utf-8")
+                        ).hexdigest(),
+                        16,
+                    )
+                )
+                + 1
+            )
+            % 2**32
+            for key in keys
+        ]
+        fit_data[model_kind] = {}
+        fit_stats[model_kind] = {}
+
+        for key, fname, seed in zip(keys, fnames, seeds):
+            data = read_data(fname)
+
+            model_data = model_module.make_model_data(
+                z=data.z,
+                nzs=data.nzs,
+                mn=data.mn,
+                cov=data.cov,
+                mn_pars=data.mn_pars,
+                zbins=data.zbins,
+                **model,
+            )
+
+            map_params = measure_map(
+                model_module=model_module,
+                model_data=model_data,
+                seed=seed,
+                progress_bar=True,
+                num_steps=50_000,
+                learning_rate=1e-2,
+            )
+
+            chi2_info = compute_model_chi2_info(
+                model_module=model_module,
+                model_data=model_data,
+                data=data,
+                params=map_params,
+            )
+
+            fit_stats[model_kind][key] = {
+                "key": key,
+                "model_kind": model_kind,
+                "chi2": float(chi2_info["chi2"]),
+                "dof": int(chi2_info["dof"]),
+                "p_value": float(chi2_info["p_value"]),
+                "seed": seed,
+            }
+            pbar.write(
+                yaml.dump(
+                    fit_stats[model_kind][key],
+                    default_flow_style=False,
+                    indent=2,
+                )
+            )
+
+            mcmc = run_mcmc(
+                model_module=model_module,
+                model_data=model_data,
+                init_params=map_params,
+                seed=seed,
+                progress_bar=True,
+                dense_mass=True,
+                num_samples=2500,
+            )
+
+            sstr = io.StringIO()
+            with contextlib.redirect_stdout(sstr), contextlib.redirect_stderr(sstr):
+                mcmc.print_summary(exclude_deterministic=False)
+            mcmc_summary = sstr.getvalue()
+            pbar.write(mcmc_summary)
+
+            fit_data[model_kind][key] = {
+                "map_params": map_params,
+                "mcmc": mcmc,
+                "model_data": model_data,
+                "data": data,
+                "seed": seed,
+            }
+
+            fit_stats[model_kind][key]["map_params"] = {
+                k: float(v) for k, v in map_params.items()
+            }
+            fit_stats[model_kind][key]["mcmc_summary"] = mcmc_summary
+
+            pbar.update(1)
+
+if WRITE:
+    with h5py.File(f"imsim_v{VERSION}_mcmc_samples.h5", "w") as fp:
+        for model in models:
+            model_kind = model["model_kind"]
+            for key in keys:
+                samples = fit_data[model_kind][key]["mcmc"].get_samples()
+                for param in samples:
+                    fp.create_dataset(
+                        f"{model_kind}-{key}/{param}",
+                        data=np.asarray(samples[param], dtype=np.float64),
+                        compression="gzip",
+                    )
+
+
+if WRITE:
+    with open(f"fit_stats_v{VERSION}.yml", "w") as f:
+        yaml.dump(fit_stats, f, default_flow_style=False, indent=2)
+
+
+desnz = np.load("../../data/Tz_realizations_WZ_bq_pile4_0d05.npy")
+
+corr_nz = {}
+with tqdm.tqdm(
+    ncols=80,
+    total=len(models) * len(keys) * 2,
+    desc="adjusting data n(z) and writing model data",
+) as pbar:
+    for skip_nn in [True, False]:
+        for model in models:
+            model_kind = model["model_kind"]
+            corr_nz[model_kind] = {}
+
+            for key in keys:
+                corr_nz[model_kind][key] = {}
+
+                mvals, dzvals, finalnzs = compute_eff_nz_from_data(
+                    model_module=model_module,
+                    mcmc_samples=fit_data[model_kind][key]["mcmc"].get_samples(),
+                    model_data=fit_data[model_kind][key]["model_data"],
+                    input_nz=desnz,
+                    rng=np.random.default_rng(fit_data[model_kind][key]["seed"]),
+                    shift_negative=skip_nn,
+                )
+                corr_nz[model_kind][key]["mvals"] = mvals
+                corr_nz[model_kind][key]["dzvals"] = dzvals
+                corr_nz[model_kind][key]["finalnzs"] = finalnzs
+
+                m_mn = np.mean(mvals, axis=0)
+                m_sd = np.std(mvals, axis=0)
+
+                dz_mn = np.mean(dzvals, axis=0)
+                dz_sd = np.std(dzvals, axis=0)
+
+                if skip_nn:
+                    fit_stats[model_kind][key]["m_mn_nn"] = [
+                        float(mval) for mval in m_mn
+                    ]
+                    fit_stats[model_kind][key]["m_sd_nn"] = [
+                        float(mval) for mval in m_sd
+                    ]
+                    fit_stats[model_kind][key]["dz_mn_nn"] = [
+                        float(dzval) for dzval in dz_mn
+                    ]
+                    fit_stats[model_kind][key]["dz_sd_nn"] = [
+                        float(dzval) for dzval in dz_sd
+                    ]
+                else:
+                    fit_stats[model_kind][key]["m_mn"] = [float(mval) for mval in m_mn]
+                    fit_stats[model_kind][key]["m_sd"] = [float(mval) for mval in m_sd]
+                    fit_stats[model_kind][key]["dz_mn"] = [
+                        float(dzval) for dzval in dz_mn
+                    ]
+                    fit_stats[model_kind][key]["dz_sd"] = [
+                        float(dzval) for dzval in dz_sd
+                    ]
+
+                pbar.write(f"""\
+model-kind|key|shift_nonneg: {model_kind}|{key}|{skip_nn}
+|--------------------------------------------|
+| bin | m [10^-3, 3sigma] | dz [1sigma]      |
+|--------------------------------------------|
+| 0   | {m_mn[0] / 1e-3:+5.1f} +/- {3 * m_sd[0] / 1e-3:-5.1f}   | {dz_mn[0]:+0.3f} +/- {dz_sd[0]:0.3f} |
+| 1   | {m_mn[1] / 1e-3:+5.1f} +/- {3 * m_sd[1] / 1e-3:-5.1f}   | {dz_mn[1]:+0.3f} +/- {dz_sd[1]:0.3f} |
+| 2   | {m_mn[2] / 1e-3:+5.1f} +/- {3 * m_sd[2] / 1e-3:-5.1f}   | {dz_mn[2]:+0.3f} +/- {dz_sd[2]:0.3f} |
+| 3   | {m_mn[3] / 1e-3:+5.1f} +/- {3 * m_sd[3] / 1e-3:-5.1f}   | {dz_mn[3]:+0.3f} +/- {dz_sd[3]:0.3f} |
+|--------------------------------------------|
+""")
+
+                pbar.update(1)
+
+        if skip_nn:
+            dfname = f"des_y6_nz_SOMPZ_WZ_imsim_v{VERSION}_nonneg.h5"
+        else:
+            dfname = f"des_y6_nz_SOMPZ_WZ_imsim_v{VERSION}.h5"
+
+        if WRITE:
+            with h5py.File(dfname, "w") as fp:
+                for model in models:
+                    model_kind = model["model_kind"]
+                    for key in keys:
+                        fp.create_dataset(
+                            f"{model_kind}-{key}/m",
+                            data=corr_nz[model_kind][key]["mvals"],
+                            compression="gzip",
+                        )
+                        fp.create_dataset(
+                            f"{model_kind}-{key}/dz",
+                            data=corr_nz[model_kind][key]["dzvals"],
+                            compression="gzip",
+                        )
+                        fp.create_dataset(
+                            f"{model_kind}-{key}/nz",
+                            data=corr_nz[model_kind][key]["finalnzs"],
+                            compression="gzip",
+                        )
+
+if WRITE:
+    with open(f"fit_stats_v{VERSION}.yml", "w") as f:
+        yaml.dump(fit_stats, f, default_flow_style=False, indent=2)
